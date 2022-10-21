62f46dfe65caf598a71a1fdf57a23b1ebf9e73e1
Eras1722 <70988877+Eras1722@users.noreply.github.com>
2022-10-21 09:16:22 -0500

    Added bone rotation

--- TrialsForAGod/Assets/Scripts/ProjBehavior.cs
+++ TrialsForAGod/Assets/Scripts/ProjBehavior.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class ProjBehavior : MonoBehaviour
 {
     private Projectile projVal;
+    //public GameObject self;
+    public int speed;
+    private float rotRate;
 
     public bool bSpin;
-    private Quaternion qStart, qEnd;
+    private Quaternion qStart; 
+    public Quaternion qEnd;
+    private Quaternion qTemp;
 
     private void Start()
     {
         projVal = GetComponent<ProjectileValueHolder>().projValues;
+        qStart = transform.rotation;
+        qTemp = qEnd;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -20,17 +27,18 @@ public class ProjBehavior : MonoBehaviour
         {
             Debug.Log("Hit the bone");
             Vector3 difference = other.transform.position - transform.position;
-            
+
+            difference.y = other.transform.position.y;
             difference = difference.normalized * projVal.projKnocback;
             other.GetComponent<PlayerHealth>().TakeDamageAndKnockback(projVal.projDamage, difference);
             //i like to delete things at the end of the frame to prevent any inconsistancies with deleting things at the same time as funtions running
             StartCoroutine(deleteProj());
             
         }
-
-        //if bone hits wall delete it as well
-
-
+        if (other.gameObject.layer == LayerMask.NameToLayer("Geo"))
+        {
+            StartCoroutine(deleteProj());
+        }
     }
     IEnumerator deleteProj()
     {
@@ -38,8 +46,15 @@ public class ProjBehavior : MonoBehaviour
         Destroy(gameObject);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        
+
+        if (bSpin)
+            rotate();
+    }
+    private void rotate()
+    {
+        rotRate += 10;
+        transform.rotation = Quaternion.Euler(new Vector3(90, rotRate, 0));
     }
 }
