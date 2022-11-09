3ba235b971c2e83e4280e8b62a2862ff70346b10
dylanvandenhemel <60535253+dylanvandenhemel@users.noreply.github.com>
2022-11-09 09:13:37 -0600

    fade in and out plus font added to other scenes

--- TrialsForAGod/Assets/Scripts/NextScene.cs
+++ TrialsForAGod/Assets/Scripts/NextScene.cs
@@ -6,13 +6,29 @@ using UnityEngine.SceneManagement;
 public class NextScene : MonoBehaviour
 {
     private int currentScene;
+    public GameObject blackScreen;
 
     private void Start()
     {
         currentScene = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(fadeInDisable());
     }
     public void ChangeScene()
     {
+        StartCoroutine(fadeAnimDelay());
+    }
+
+    IEnumerator fadeInDisable()
+    {
+        blackScreen.GetComponent<Animator>().SetTrigger("fadeIn");
+        yield return new WaitForSeconds(1);
+        //blackScreen.SetActive(false);
+    }
+    IEnumerator fadeAnimDelay()
+    {
+        //blackScreen.SetActive(true);
+        blackScreen.GetComponent<Animator>().SetTrigger("fadeOut");
+        yield return new WaitForSeconds(1);
         SceneManager.LoadScene(currentScene + 1);
     }
 }
