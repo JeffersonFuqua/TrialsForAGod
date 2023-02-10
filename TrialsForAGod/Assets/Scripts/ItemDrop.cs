f317b77125db33b6c73682215eae66bc3bf801ae
Eras1722 <70988877+Eras1722@users.noreply.github.com>
2023-02-10 09:12:27 -0600

    Made progress on item drop

--- TrialsForAGod/Assets/Scripts/ItemDrop.cs
+++ TrialsForAGod/Assets/Scripts/ItemDrop.cs
@@ -9,10 +9,20 @@ public class ItemDrop : MonoBehaviour
     [Range (0, 100)]
     public float dropRate;
 
+    public void OnEnable()
+    {
+        EnemyHealth.DropItem += DropItem;
+    }
 
+    public void OnDisable()
+    {
+        EnemyHealth.DropItem -= DropItem;
+    }
 
     public void DropItem()
     {
-
+        Instantiate(itemToDrop);
+        itemToDrop.transform.position = this.transform.position;
+        Debug.Log("Drop");
     }
 }
