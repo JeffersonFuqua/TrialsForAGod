c668788b4a3e63f5d5917453dfd8fea0563ca339
Eras1722 <70988877+Eras1722@users.noreply.github.com>
2023-03-01 10:00:04 -0600

    Started Gallery image swap

--- TrialsForAGod/Assets/Scripts/MainMenu/GalleryScript.cs
+++ TrialsForAGod/Assets/Scripts/MainMenu/GalleryScript.cs
@@ -8,22 +8,49 @@ public class GalleryScript : MonoBehaviour
     public UIDocument addUI;
     private VisualElement rootElement;
     private Button tester;
-
+    private Button testerTwo;
+    public GalleryImages gPics;
+    private int x = 0;
+    /*
+     * Make new scriptable object to hold all images X
+     * See speakervalues ares pics for how to reference it
+     * make multiple buttons and assign them a value for the reference in the all image list
+     * probably should use a foreach loop if possible but I don't think it is 
+     */
     public void OnEnable()
     {
         rootElement = addUI.rootVisualElement;
         tester = rootElement.Q<Button>("AresAngry");
+       // Debug.Log(tester.style.backgroundImage.value.sprite.name);
+        testerTwo = rootElement.Q<Button>("Display");
         Debug.Log("Enabled");
-        Enlarge();
+        Startup();
     }
 
-    public void Enlarge()
+    public void Startup()
     {
-        Debug.Log("Enlarge");
-        tester.clicked += () => log();
+        Debug.Log("Started");
+        tester.clicked += () => Enlarge();
+        testerTwo.clicked += () => HideLarge();
     }
-    public void log()
+    public void Enlarge()
+    { 
+        testerTwo.style.backgroundImage = gPics.imageList[0];
+       // testerTwo.style.backgroundColor = Color.black;
+        testerTwo.BringToFront();
+        testerTwo.SetEnabled(true);
+    }
+    public void HideLarge()
     {
-        Debug.Log("clicked");
+        Debug.Log("Hide");
+        testerTwo.style.backgroundImage = null;
+        testerTwo.style.backgroundColor = Color.clear;
+        testerTwo.SendToBack();
+        testerTwo.SetEnabled(false);
+    }
+    public void OnDisable()
+    {
+        tester.clicked -= () => Enlarge();
+        testerTwo.clicked -= () => HideLarge();
     }
 }
