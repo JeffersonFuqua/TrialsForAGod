702a64eaca88dc30f0ba5eee28745d74a3c5ec60
Eras1722 <70988877+Eras1722@users.noreply.github.com>
2023-03-22 08:28:54 -0500

    Fixing coins updated Gallery List

--- TrialsForAGod/Assets/ScriptableObjects/GalleryImages.cs
+++ TrialsForAGod/Assets/ScriptableObjects/GalleryImages.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 [CreateAssetMenu]
 public class GalleryImages : ScriptableObject
 {
-    public List<Texture2D> imageList;
+    public List<Sprite> imageList;
 
 }
--- TrialsForAGod/Assets/Scripts/ItemDrop.cs
+++ TrialsForAGod/Assets/Scripts/ItemDrop.cs
@@ -6,18 +6,33 @@ public class ItemDrop : MonoBehaviour
 {
     public GameObject commonDrop;
     public GameObject rareDrop;
+    public Vector3 dropSpot;
 
     public int dropAmountLow, dropAmountHigh;
+    private int dropAmount;
 
-    [Range (0, 100)]
+    [Range(0, 100)]
     public float rareDropRate;
+
+    public void Start()
+    {
+        dropAmount = Random.Range(dropAmountLow, dropAmountHigh);
+    }
+
+    public void SetDrop(Vector3 deathSpot)
+    {
+        dropSpot.x = deathSpot.x + Random.Range(-0.2f, 0.2f);
+        dropSpot.y = commonDrop.transform.position.y;
+        dropSpot.z = deathSpot.z + Random.Range(-0.2f, 0.2f);
+    }
+
     public void DropItem()
     {
-        for(int dropLoop = 0; dropLoop < Random.Range(dropAmountLow, dropAmountHigh); dropLoop++)
+        for(int dropLoop = 0; dropLoop < dropAmount; dropLoop++)
         {
             Instantiate(commonDrop);
             commonDrop.transform.position = this.transform.position;
-            commonDrop.transform.position = new Vector3(commonDrop.transform.position.x + Random.Range(-0.5f, 0.5f), commonDrop.transform.position.y, commonDrop.transform.position.z + Random.Range(-0.5f, 0.5f));
+            commonDrop.transform.position = new Vector3(commonDrop.transform.position.x + Random.Range(-0.2f, 0.2f), commonDrop.transform.position.y, commonDrop.transform.position.z + Random.Range(-0.1f, 0.1f));
             Debug.Log("We be droppin");
             if (commonDrop != enabled)
                 commonDrop.SetActive(true);
@@ -26,6 +41,7 @@ public class ItemDrop : MonoBehaviour
         {
             Instantiate(rareDrop);
             rareDrop.transform.position = this.transform.position;
+            commonDrop.transform.position = new Vector3(commonDrop.transform.position.x + Random.Range(-0.2f, 0.2f), commonDrop.transform.position.y, commonDrop.transform.position.z + Random.Range(-0.2f, 0.2f));
             if (rareDrop != enabled)
                 rareDrop.SetActive(true);
             Debug.Log("Rare!");
