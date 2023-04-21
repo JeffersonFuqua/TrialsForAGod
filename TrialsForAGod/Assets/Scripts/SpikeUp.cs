24a3dda8f9ca7e4b5f1518169427f492dd397ab7
Eras1722 <70988877+Eras1722@users.noreply.github.com>
2023-04-21 07:57:23 -0500

    Added pause controller to CFallen

---
+++ TrialsForAGod/Assets/Scripts/SpikeUp.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeUp : MonoBehaviour
+{
+    public GameObject spikes;
+    private bool bTriggered;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!bTriggered)
+            StartCoroutine(nameof(spikeUpDelay));
+        bTriggered = true;
+    }
+
+    IEnumerator spikeUpDelay()
+    {
+
+        yield return new WaitForSeconds(0.1f);
+        spikes.SetActive(true);
+        StartCoroutine(nameof(spikeDownDelay));
+    }
+
+    IEnumerator spikeDownDelay()
+    {
+        yield return new WaitForSeconds(0.3f);
+        spikes.SetActive(false);
+        bTriggered = false;
+    }
+}
