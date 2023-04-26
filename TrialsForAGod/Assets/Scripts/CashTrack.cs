642862350bf6c92ff57e4b29eea71b1690dc03b5
Eras1722 <70988877+Eras1722@users.noreply.github.com>
2023-04-26 08:36:41 -0500

    Coins now save over
    Still never used for anything

--- TrialsForAGod/Assets/Scripts/CashTrack.cs
+++ TrialsForAGod/Assets/Scripts/CashTrack.cs
@@ -2,27 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CashTrack : MonoBehaviour
 {
     public TextMeshProUGUI coinDisplay;
-    private float coins;
+    private static float coins;
+    private float savedCoins;
 
-    private void Start()
-    {
-        coins = 0; //Will need to set it to another amount stored in the player SO
-    }
     private void OnEnable()
     {
         PickUps.UpdateCash += UpdateCoins;
+        if(SceneManager.GetActiveScene().name == "Sewer Tutorial")
+        {
+            Debug.Log(SceneManager.GetActiveScene().name);
+            coins = 0;
+            savedCoins = 0;
+        }
+        coinDisplay.text = coins.ToString();
     }
     private void OnDisable()
     {
         PickUps.UpdateCash -= UpdateCoins;
+        coins = savedCoins;
     }
+    
     public void UpdateCoins(float cashflow)
     {
         coins += cashflow;
         coinDisplay.text = coins.ToString();
+        savedCoins = coins;
     }
 }
